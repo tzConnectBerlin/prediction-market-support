a2a753dcdf47c9c43984993ee3dfb9be1dd6412c
John Newby <john@newby.org>
2021-01-27 14:06:26 +0100

    Small fixes

--- summary.py
+++ summary.py
@@ -27,19 +27,23 @@ def load_json(url):
         return data
 
 def get_question_data(data):
+    def get_value(js):
+        if js.get('value') != None:
+            return js['value']
+        if js.get('children') != None:
+            return get_question_data(js['children'])
     ls = list(map(lambda x: {
-        x['name'] : x.get('value'),
+        x['name'] : get_value(x),
         }, data))
     return dict(ChainMap(*ls))
 
 def get_questions(id):
-    url = f"{bcd_url}bigmap/{network}/{id}/keys"
-    print(url)
+    url = f"{bcd_url}bigmap/{network}/{id}/keys?size=10000"
     js = load_json(url)
     ls = list(map(lambda x : {
         x['data']['key']['value'] : get_question_data(x['data']['value']['children'])
     }, js))
-    return ls
+    return jq.first('add', ls)
 
 def get_storage_internal(js):
     def get_value(js):
@@ -50,17 +54,17 @@ def get_storage_internal(js):
     return dict(list(map(lambda x :( x['name'], get_value(x) ), js)))
 
 def get_storage(id):
-    url = f"{bcd_url}/contract/{network}/{id}/storage"
+    url = f"{bcd_url}/contract/{network}/{id}/storage?size=10000"
     js = load_json(url)
-    storage = get_storage_internal(js)
+    storage = get_storage_internal(js['children'])
     return storage
 
 def get_ledger(id):
-    url = f"{bcd_url}bigmap/{network}/{id}/keys"
+    url = f"{bcd_url}bigmap/{network}/{id}/keys?size=10000"
     js = load_json(url)
-    return jq.all('map({ "\(.data.key.children[0].value).\(.data.key.children[1].value)": .data.value.children[0].value }) | add', js)
+    return jq.first('map({ "\(.data.key.children[0].value).\(.data.key.children[1].value)": .data.value.children[0].value }) | add', js)
 
 def get_total_supply(id):
-    url = f"{bcd_url}bigmap/{network}/{id}/keys"
+    url = f"{bcd_url}bigmap/{network}/{id}/keys?size=10000"
     js = load_json(url)
     return jq.all('map({ (.data.key.value): (.data.value.value) }) | add', js)
