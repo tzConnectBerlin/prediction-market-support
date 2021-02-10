728204b9f58e247e5428d90875f3a1d6869b4a2d
multi prise <korokoko.toi@gmail.com>
2021-02-10 09:29:49 +0100

    added comment and global for time

--- demo.py
+++ demo.py
@@ -17,6 +17,9 @@ from datetime import datetime, timedelta
 ##### Local Script 
 import summary
 
+AUCTION_END_DATE=5
+MARKET_END_DATE=50
+
 ##################
 # Setup
 ##################
@@ -39,7 +42,7 @@ for user in users:
     accounts[user] = pytezos.using(
             key = f"users/{user}.json",
             shell = "http://tezos.newby.org:8732",
-            )
+    )
     #if args.activate_accounts:
     #    accounts[user].activate_account().autofill().sign().inject()
     #if args.reveal_accounts:
@@ -57,12 +60,18 @@ for user in users:
 ##################
 @app.command()
 def create_question(question: str, answer: str, user: str):
-    """Create a question in IPFS"""
+    """
+    Create a question in IPFS
+
+    question: String representing the answer asked
+    answer: String representing the possible answer
+    user: String representing the questions owner
+    """
     if user not in users:
         raise Exception("Missing User");
     timenow = datetime.now().astimezone(pytz.utc)
-    auction_end_date = timenow + timedelta(minutes=5)
-    market_close_date = timenow + timedelta(minutes=50)
+    auction_end_date = timenow + timedelta(minutes=AUCTION_END_DATE)
+    market_close_date = timenow + timedelta(minutes=MARKET_END_DATE)
     param = {
             'question': question,
             'yesAnswer': answer,
@@ -80,13 +89,13 @@ def create_question(question: str, answer: str, user: str):
         'market_close': int(market_close_date.timestamp()),
         'rate': random.randint(1,99) * PERCENT,
         'quantity': 100,
-
-        }).operation_group.autofill().sign().inject()
+    }).operation_group.autofill().sign().inject()
     print(f"Created market {ipfs_hash} in PM contract")
     return ipfs_hash
 
 @app.command()
 def fund_stablecoin():
+    """ Fund all accounts with a random quantity of tezos """
     for user in users:
         print(f"Transferring to {user}")
         admin_account = summary.admin_account()
@@ -101,6 +110,7 @@ def fund_stablecoin():
 
 @app.command()
 def transfer_stablecoin(dest):
+    """ Transfer a certain amount of coins toward an user address"""
     admin_account = summary.admin_account()
     stablecoin = admin_account.contract(summary.get_storage(config['Tezos']['pm_contract'])['stablecoin'])
     stablecoin.transfer({
@@ -111,13 +121,20 @@ def transfer_stablecoin(dest):
 
 @app.command()
 def bid_auction(ipfs_hash: str, user:str , quantity: int, rate: int):
-    rate
+    """
+    Launch a bid on an auction
+    ipfs_hash: the contract concerned by the bid
+    user: string representing the user which is bidding during the auction
+    quantity: Integer representing quantity of stable coins bid during the auction
+    rate: What is rate?
+    """
     print(f"User {user} bidding {rate} on {ipfs_hash}")
     d = dict(os.environ)
+    address_user = pm_contracts[user]
     d['AUCTION'] = ipfs_hash
     d['HOST'] = config['Tezos']['node']
     d['PORT'] = config['Tezos']['port']
-    d['ACCOUNT'] = user
+    d['ACCOUNT'] = address_user
     d['CONTRACT'] = config['Tezos']['pm_contract']
     d['RATE'] = str(rate)
     d['QUANTITY'] = "50000000000000000000"
@@ -134,14 +151,23 @@ def bid_auction(ipfs_hash: str, user:str , quantity: int, rate: int):
 
 @app.command()
 def random_bids(ipfs_hash):
+    """
+    Launch random bid on a auction from all of the user contained in the users folder
+
+    ipfs_hash: Contract on which the bid are made
+    """
+    if len(users) == 0:
+        print("Please add some users before using this functionality")
     for user in users:
+        print(pm_contracts[user])
         rate = random.randint(1,99) * PERCENT
+        address_user = pm_contracts[user].address
         print(f"User {user} bidding {rate} on {ipfs_hash}")
         d = dict(os.environ)
         d['AUCTION'] = ipfs_hash
         d['HOST'] = config['Tezos']['node']
         d['PORT'] = config['Tezos']['port']
-        d['ACCOUNT'] = user
+        d['ACCOUNT'] = address_user
         d['CONTRACT'] = config['Tezos']['pm_contract']
         d['RATE'] = str(rate)
         d['QUANTITY'] = "50000000000000000000"
@@ -152,13 +178,18 @@ def random_bids(ipfs_hash):
                 'rate': rate,
                 'quantity': 50000000000000000000,
         }
-         #Not working
-        print(data)
-        pm_contracts[user].bid(data).operation_group.autofill(gas_reserve=200000).sign().inj
+        print("************************************<")
+        #print(pm_contracts[user].bid(data).operation_group.fill())
+        #print(pm_contracts[user].bid(data).operation_group.sign())
+        print("************************************>")
+        #Not working
+        #print(data)
+        pm_contracts[user].bid(data).operation_group.fill().sign().inject()
 
 
 @app.command()
 def close_auction(ipfs_hash: str):
+    """ Close the auction """
     pm_contracts[users[2]].closeAuction(ipfs_hash).operation_group.autofill().sign().inject()
 
 if __name__ == "__main__":
--- utils/get_country_and_capital.py
+++ utils/get_country_and_capital.py
@@ -1,12 +1,13 @@
 import json
+import os
 import random
 
 def get_country_and_capital():
-    """ Return random country and capital combo"""
-    with open('country-by-capital-city.json', 'r') as f:
+    """ Return random country and capital combination"""
+    with open('utils/country-by-capital-city.json', 'r') as f:
         data = json.load(f)
         count = len(data)
-        #print(data[random.randint(0,count)])
+        print(data[random.randint(0,count)])
         return data[random.randint(0,count)]
 
 get_country_and_capital()
