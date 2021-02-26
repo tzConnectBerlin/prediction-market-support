f17921b621c88e4e6a9a3eb4f7589d42a9e9fece
multi prise <korokoko.toi@gmail.com>
2021-02-26 02:44:48 +0100

    added count

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,17 +1,24 @@
-from src.utils import summary
-
+from pytezos import pytezos
 
 def get_stablecoin(account, contract: str):
     """
     Return an reference to the stablecoin storage  for account
     """
-    stablecoin = account.contract(
-        summary.get_storage(contract)['stablecoin']
-    )
-    return stablecoin
+    stablecoin_contract = account.contract(contract)\
+            .storage["stablecoin"]()
+    stablecoin_client = account.contract(stablecoin_contract)
+    return stablecoin_client
 
 def get_public_key(account):
     """
     Get public key hash from account
     """
     return account.key.public_key_hash()
+
+def get_count(contract, account):
+    """
+    Get count from contract
+    """
+    return contract.shell.head.context \
+            .contracts[account].count()
+
