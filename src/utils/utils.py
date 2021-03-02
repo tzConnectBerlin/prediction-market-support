618cf3c77ad0b7707cc1a8bebcbffb993876cc4c
multi prise <korokoko.toi@gmail.com>
2021-03-02 07:57:31 +0100

    added error messages and callback for util function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -26,7 +26,7 @@ def submit_transaction(transaction, account, count=None, tries=None, error_func=
         error_message = ast.literal_eval(str(e)[1:-2])
         splited_error_message = error_message["id"].split('.')
         if error_func != None:
-            error_func(splited_error_message)
+            error_func(error_message)
         print(splited_error_message[3])
         print(e)
         #raise
